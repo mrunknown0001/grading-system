4be5029afdb394e5ca9b35ca85dfec390becd033
adam <madamt0001@gmail.com>
2017-11-07 15:53:25 +0800

    udpate

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -32,7 +32,7 @@
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
-<body>
+<body class="">
 	<header class="main-header">
 		<h3 class="text-center">Welcome to Concepcion Catholic School - Student Grading System</h3>
 		
