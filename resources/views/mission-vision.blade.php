d63b40711d50c9fbe4dac4980b0e4f816402ed10
adam <madamt0001@gmail.com>
2017-11-12 09:50:19 +0800

    update mission and vision

--- resources/views/mission-vision.blade.php
+++ resources/views/mission-vision.blade.php
@@ -8,14 +8,14 @@
     <hr>
     <hr>
     <div style="margin-top: 50px; padding: 25px; text-align: justify;">
-      <h3 class="text-center">Mission</h3>
+      <h3 class="text-center">Vision</h3>
       <h4>&nbsp;&nbsp;&nbsp;Concepcion Catholic School is a Filipino Catholic institution dedicated to quiality education, animated by the Spirit of the Gospel and inspired by the examples of the Blessed Virgin Mary and St. Dominic.</h4>
       <hr>
       <div class="row">
           <div class="col-md-3"></div>
           <div class="col-md-6">
           
-          <h3 class="text-center">Our Mission</h3>
+          <h3 class="text-center">Mission</h3>
           <ul class="disk">
             <li><h4>To develop Christian and moral values</h4></li>
             <li><h4>To provide quality education to our pupils and students</h4></li>
